ae238a121d5f2b6527a0be1c5e9ad945634c60fe
Russell Jarvis <rjjarvis@asu.edu>
2017-07-02 13:33:46 +1000

    actual dev changes

--- examples/l5pc/benchmark/start.sh
+++ examples/l5pc/benchmark/start.sh
@@ -12,18 +12,20 @@ export L5PCBENCHMARK_USEIPYP=1
 export IPYTHONDIR="`pwd`/.ipython"
 export IPYTHON_PROFILE=benchmark.${SLURM_JOBID} 
 # ipython profile create --profile=${IPYTHON_PROFILE}
-ipcontroller --init --ip='*' --sqlitedb --profile=${IPYTHON_PROFILE} &
+ipcontroller --init --ip='*' --profile=${IPYTHON_PROFILE} &
 sleep 10
-ipengine --profile=${IPYTHON_PROFILE} &
-sleep 1
-CHECKPOINTS_DIR="checkpoints/run.${SLURM_JOBID}"
-mkdir -p ${CHECKPOINTS_DIR}
+ipengine --profile=${IPYTHON_PROFILE} 
+#sleep 1
+#CHECKPOINTS_DIR="checkpoints/run.${SLURM_JOBID}"
+#mkdir -p ${CHECKPOINTS_DIR}
 
-pids=""
-for seed in `seq 1 4`
-do
-    BLUEPYOPT_SEED=${seed} python opt_l5pc.py --offspring_size=${OFFSPRING_SIZE} --max_ngen=${MAX_NGEN} --start --checkpoint "${CHECKPOINTS_DIR}/seed${seed}.pkl" &
-    pids="${pids} $!"
-done
+#pids=""
+#for seed in `seq 1 4`
+#do
+#    BLUEPYOPT_SEED=${seed} python opt_l5pc.py --offspring_size=${OFFSPRING_SIZE} --max_ngen=${MAX_NGEN} --start --checkpoint "${CHECKPOINTS_DIR}/seed${seed}.pkl" &
+#    pids="${pids} $!"
+#done
 
-wait $pids
+mpiexec -np 4 python opt_l5pc.py # --start
+
+#wait $pids
