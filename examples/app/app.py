8aadd4c3dc0a34713b8bc8076c1cfd8dca46c1ab
Russell <russelljarvis@protonmail.com>
2020-08-24 13:40:04 +1000

    movie

--- examples/app/app.py
+++ examples/app/app.py
@@ -1,7 +1,4 @@
-"""
-Process-sleep-data.py
-Processes fitbit sleep data files, uploads data into fitbit dataframe, outputs graphs.
-"""  	
+
 
 import pandas as pd
 import numpy as np                       
@@ -24,6 +21,7 @@ from neuronunit.optimisation.model_parameters import MODEL_PARAMS
 from neuronunit.optimisation.optimization_management import inject_and_plot_model, inject_and_plot_passive_model
 
 
+import pandas as pd
 
 from neuronunit.optimisation.optimization_management import TSD
 
@@ -47,18 +45,34 @@ def instance_opt(experimental_constraints,MODEL_PARAMS,test_key,model_value,MU,N
           cxpb=cxp)
 
   final_pop, hall_of_fame, logs, hist = optimisation.run(max_ngen=NGEN)
-
-  st.success("Model fitted to experiment {0}".format(test_key))
-
   best_ind = hall_of_fame[0]
+
+  
+  #self_evaluate
+  st.success("Model best fit to experiment {0}".format(test_key))
+  st.markdown('\n\n\n\n')
+
+  best_ind_dict = cell_evaluator.param_dict(best_ind)
   model = cell_evaluator.cell_model
   cell_evaluator.param_dict(best_ind)
   model.attrs = {str(k):float(v) for k,v in cell_evaluator.param_dict(best_ind).items()}
   opt = model.model_to_dtc()
+  opt.attrs = {str(k):float(v) for k,v in cell_evaluator.param_dict(best_ind).items()}
+  opt.tests = experimental_constraints
+  obs_preds = opt.make_pretty(experimental_constraints)
+
+  frame = opt.SA.to_frame()
+  st.write(frame.T)
+
+  obs_preds = opt.obs_preds
+  st.write(obs_preds.T)
+
   vm,fig = inject_and_plot_model(opt,plotly=True)
   st.write(fig)
-  #vm,fig = inject_and_plot_passive_model(opt,opt,plotly=True)
-  #st.write(fig)
+
+  #inject_and_plot_passive_model(opt,plotly=Tru)
+  fig = inject_and_plot_passive_model(opt,opt,plotly=True)
+  st.write(fig)
 
   #plt.show()
   st.markdown("""
@@ -74,24 +88,29 @@ def instance_opt(experimental_constraints,MODEL_PARAMS,test_key,model_value,MU,N
   """.format(sum(best_ind.fitness.values),10*len(experimental_constraints)))
 
 if __name__ == "__main__":  
-    st.title('Reduced Model Optimization')
+    st.title('Reduced Model Fitting to Data')
     experimental_constraints.pop("Olfactory bulb (main) mitral cell")
     #API_TOKEN = st.text_input('Please Enter Your Fitbit API token:')
     test_key = st.sidebar.radio("\
       What experiments would you like to fit models to?"
 		,tuple(experimental_constraints.keys()))
-    #names = tuple(t.name for t in experimental_constraints[test_key].tests)
-    #st.text(names)
+
 
 
     experimental_constraints = TSD(experimental_constraints[test_key])
-    test_keys = st.sidebar.multiselect("\
-     Are you interested in less than all of the features?"
-		,tuple(experimental_constraints.keys()))
+    
+    test_keys = list(experimental_constraints.keys())
+    subset_tests = st.sidebar.radio("\
+		Would you like to fit models on a subset of tests?"
+		,("No","Yes"))
+    if subset_tests == "Yes":
+      test_keys = st.sidebar.multiselect("\
+      Are you interested in less than all of the features?"
+      ,tuple(experimental_constraints.keys()))
 
-    #st.text()
 
     experimental_constraints = [ experimental_constraints[k] for k in test_keys ]
+    #st.text(test_keys)
     model_value = st.sidebar.radio("\
 		Which models would you like to optimize"
 		,("ADEXP","IZHI","NEURONHH"))
@@ -100,7 +119,7 @@ if __name__ == "__main__":
 		Do you want diverse solutions or just the best solution?"
 		,("NSGA2","IBEA"))
 
-    readiness = st.sidebar.radio("\
+    readiness = st.radio("\
 		Ready to go?"
 		,("No","Yes"))
 
@@ -113,14 +132,41 @@ if __name__ == "__main__":
 
     if readiness == "Yes":
       instance_opt(experimental_constraints,MODEL_PARAMS,test_key,model_value,MU,NGEN)
+      
+     
+      if model_value == "ADEXP":
+        st.markdown('''
+        In the {2} Model:
+        Over three different current injection strengths mean Model speed was {0}
+        ms, to find unkown rheoase value it takes {1} seconds'''.format(300,3.8,model_value))
+
+
+      if model_value == "IZHI":
+        st.markdown('''
+        The IZHI model is the fastest.
+        Over three different current injection strengths mean Model speed was {0}
+        ms, to find unkown rheoase value it takes {1} seconds'''.format(3.36,0.7,model_value))
+
+
+      if model_value == "NEURONHH":
+        st.markdown('''
+        In the {2} Model:
+        over three different current injection strengths mean Model speed was {0}
+        ms, to find unkown rheoase value it takes {1} seconds'''.format(3.36,0.7,model_value))
+
+
+ 
+      '''
+      # Do you want to try again on a different model to see how that would look?
       '''
 
-      Do you want to try again on a different model to see how that would look?
-      '''
-
-      another_go = st.sidebar.radio("\
-		  Ready to go?"
+      another_go = st.radio("\
+		  Want to keep model on screen and compare to new model?"
 		  ,("No","Yes"))
-      if another_go:
+
+      new_model_value = st.sidebar.radio("\
+      Which models would you like to optimize"
+      ,("ADEXP","IZHI","NEURONHH"))
+      if another_go == "Yes" and (new_model_value is not model_value):
         instance_opt(experimental_constraints,MODEL_PARAMS,test_key,model_value,MU,NGEN)
 
